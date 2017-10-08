911b11dea4846abadc7a477d536871eef5c47518
John Timothy Balint <jbalint2@gmu.edu>
2017-10-08 21:29:08 +0200

    Removed Speak mysql
    Removed the Speak mysql from tutorial 6 because it wasn't in the wiki
    itself

--- Documentation/Tutorials/Source/Tutorial6/speak_insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO action (act_name,act_appl_cond,act_term_cond,act_prep_spec,act_exec_steps,act_obj_num) values ('Speak','../PAR/actions/Speak.py','../PAR/actions/Speak.py','../PAR/actions/Speak.py','../PAR/actions/Speak.py',-1);
-INSERT INTO action_parent ((SELECT act_id from action where act_name = 'Speak'),8);
